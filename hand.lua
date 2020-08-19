4ddc3ced6b1b436e66b76f0c466b56f155ce86f8
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-08-19 18:30:56 +0200

    just some optimizations

--- hand.lua
+++ hand.lua
@@ -7,6 +7,8 @@ function Hand:new(o)
   setmetatable(o, self)
   self.__index = self
   o.model = lovr.headset.newModel(o.device)
+  o.from = lovr.math.newVec3()
+  o.to = lovr.math.newVec3()
   return o
 end
 function Hand:draw()
@@ -22,7 +24,7 @@ function Hand:draw()
   end
 end
 function Hand:update()
-  local handPos = lovr.math.newVec3(lovr.headset.getPosition(self.device))
+  local handPos = lovr.math.vec3(lovr.headset.getPosition(self.device))
   if handPos.x ~= handPos.x then
     return
   end
@@ -30,8 +32,8 @@ function Hand:update()
   local handRotation = lovr.math.mat4():rotate(lovr.headset.getOrientation(self.device))
   local pointedDirection = handRotation:mul(straightAhead)
   local distantPoint = lovr.math.newVec3(pointedDirection):mul(10):add(handPos)
-  self.from = handPos
-  self.to = distantPoint
+  self.from:set(handPos)
+  self.to:set(distantPoint)
 
   local highlightedItem = nil
   Hand.letters.world:raycast(self.from.x, self.from.y, self.from.z, self.to.x, self.to.y, self.to.z, function(shape, hx, hy, hz)
@@ -40,22 +42,22 @@ function Hand:update()
   end)
   if self.highlightedItem ~= highlightedItem then 
     lovr.headset.vibrate(self.device, 0.5, 0.05) 
-    if self.highlightedItem and self.highlightedItem.dehighlight then self.highlightedItem:dehighlight() end
-    if highlightedItem and highlightedItem.highlight then highlightedItem:highlight() end
+    if self.highlightedItem and self.highlightedItem.dehighlight then self.highlightedItem:dehighlight(self) end
+    if highlightedItem and highlightedItem.highlight then highlightedItem:highlight(self) end
     self.highlightedItem = highlightedItem
   end
 
   if lovr.headset.isDown(self.device, "trigger") and self.highlightedItem ~= nil and self.selectedItem == nil then
-    if self.highlightedItem.select then self.highlightedItem:select() end
+    if self.highlightedItem.select then self.highlightedItem:select(self) end
     self.selectedItem = self.highlightedItem
     lovr.headset.vibrate(hand, 0.7, 0.2, 100)
   end
   if not lovr.headset.isDown(self.device, "trigger") and self.selectedItem ~= nil then
     if self.highlightedItem == self.selectedItem then
       lovr.headset.vibrate(hand, 0.7, 0.2, 100)
-      if self.selectedItem.actuate then self.selectedItem:actuate() end
+      if self.selectedItem.actuate then self.selectedItem:actuate(self) end
     end
-    if self.selectedItem.deselect then self.selectedItem:deselect() end
+    if self.selectedItem.deselect then self.selectedItem:deselect(self) end
     self.selectedItem = nil
   end
 end
