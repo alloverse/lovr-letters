618f16eb654cef1a0e000a4475980db25134fc1f
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-08-19 18:04:44 +0200

    callback null checks on hand

--- hand.lua
+++ hand.lua
@@ -6,12 +6,20 @@ function Hand:new(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
+  o.model = lovr.headset.newModel(o.device)
   return o
 end
 function Hand:draw()
-  lovr.graphics.setColor(0.3, 0.3, 0.3)
-  lovr.graphics.box('fill', self.from, .03, .04, .06, lovr.headset.getOrientation(self.device))
-  lovr.graphics.line(self.from, self.to)
+  if not lovr.headset.isTracked(self.device) then return end
+  if self.model then
+    model:draw(mat4(lovr.headset.getPose(hand)))
+  else
+    lovr.graphics.setColor(0.5, 0.7, 0.5)
+    lovr.graphics.box('fill', self.from, .03, .04, .06, lovr.headset.getOrientation(self.device))
+  end
+  if self.from and self.to then
+    lovr.graphics.line(self.from, self.to)
+  end
 end
 function Hand:update()
   local handPos = lovr.math.newVec3(lovr.headset.getPosition(self.device))
@@ -32,22 +40,22 @@ function Hand:update()
   end)
   if self.highlightedItem ~= highlightedItem then 
     lovr.headset.vibrate(self.device, 0.5, 0.05) 
-    if self.highlightedItem then self.highlightedItem:dehighlight() end
-    if highlightedItem then highlightedItem:highlight() end
+    if self.highlightedItem and self.highlightedItem.dehighlight then self.highlightedItem:dehighlight() end
+    if highlightedItem and highlightedItem.highlight then highlightedItem:highlight() end
     self.highlightedItem = highlightedItem
   end
 
   if lovr.headset.isDown(self.device, "trigger") and self.highlightedItem ~= nil and self.selectedItem == nil then
-    self.highlightedItem:select()
+    if self.highlightedItem.select then self.highlightedItem:select() end
     self.selectedItem = self.highlightedItem
     lovr.headset.vibrate(hand, 0.7, 0.2, 100)
   end
   if not lovr.headset.isDown(self.device, "trigger") and self.selectedItem ~= nil then
     if self.highlightedItem == self.selectedItem then
       lovr.headset.vibrate(hand, 0.7, 0.2, 100)
-      self.selectedItem:actuate()
+      if self.selectedItem.actuate then self.selectedItem:actuate() end
     end
-    self.selectedItem:deselect()
+    if self.selectedItem.deselect then self.selectedItem:deselect() end
     self.selectedItem = nil
   end
 end
